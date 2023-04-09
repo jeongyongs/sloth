49494841b7aed5f719104b26aedacc3990825f88
Lee Jeongyong <hua9378@naver.com>
2023-04-10 00:51:52 +0900

    chore: Make structure of project
    front-end: react
    back-end: spring boot

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sloth
-Web application to help handover
+This project is a web application to help handover.
+
+## Front-end
+Stack: `HTML` `CSS` `Javascript` `React`
+
+## Back-end
+Stack: `Spring Boot` `JPA` `MySQL`
---
+++ back-end/src/main/java/com/sloth/BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.sloth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
