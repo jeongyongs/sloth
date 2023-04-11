d411fd871ee2488c6fe95a8985c83b797c2baec9
Lee Jeongyong <hua9378@naver.com>
2023-04-11 17:55:03 +0900

    chore: Initialize project

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={
+                    <h1>main</h1>
+                }/>
+                <Route path="/app" element={
+                    <h1>app</h1>
+                }/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
